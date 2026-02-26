b6cc8fba2988f2fe5edcca4fc5c29cd081f2c7da
Serhii Tarasiuk <serhiy.slm@gmail.com>
2026-02-26 23:31:19 +0200

    Add hw2

---
+++ hw/hw02/SerhiiTarasiuk/Task_1.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    internal class Task_1
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write("Enter a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Enter b = ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Performing calculations:");
+            Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
+            Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+
+            if (b != 0)
+            {
+                Console.WriteLine("{0} / {1} = {2}", a, b, (double)a / b);
+            }
+            else
+            {
+                Console.Write("{0} / {1} = ", a, b);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("......NO NO NO. \n This is a long story about dividing by zero... I don't have time to explain it.");
+            }
+        }
+    }
+}
---
+++ hw/hw02/SerhiiTarasiuk/Task_5.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp1
+{
+    internal class Task_5
+    {
+        static void Main(string[] args)
+        {
+            static int ReadInt()
+            {
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    
+                    if (int.TryParse(input, out int result) && result > 0)
+                    {
+                        return result;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("NO NO NO... Only positive digits. Try again");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Please enter a side of the square A:");
+            int sideA = ReadInt();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Performing calculations.... \n" +
+                "Your square's area is: " + (sideA * sideA) + "\n" +
+                "Your square's perimeter is: " + (4 * sideA));
+        }
+    }
+}
