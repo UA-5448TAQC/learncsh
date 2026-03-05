1e1fc9f9287fe8256d387a07f35649d6ee72994c
HorlovaSofiia <sofgorlova3@gmail.com>
2026-03-05 17:19:39 +0200

    HW4

---
+++ hw/hw04/HorlovaSofiia/ConsoleApp1/Task10.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal static class Task10
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Task 10 — Contains only odd digits");
+
+            int number = Math.Abs(InputHelper.ReadInt("Enter integer:"));
+
+            bool onlyOdd = true;
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+
+                if (digit % 2 == 0)
+                {
+                    onlyOdd = false;
+                    break;
+                }
+
+                number /= 10;
+            }
+
+            if (number == 0 || onlyOdd == false)
+            {
+                ConsoleHelper.WriteError("Not only odds");
+                InputHelper.PrintSeparator();
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"Contains only odd digits: {onlyOdd}");
+                InputHelper.completedTasks++;
+            }
+            InputHelper.PrintSeparator();
+        }
+    }
+}
---
+++ hw/hw04/HorlovaSofiia/ConsoleApp1/Task5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal static class Task5
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Task 5 — Print every second character");
+
+            string text = InputHelper.ReadNonEmptyString("Enter text:");
+
+            if (text.Length < 2)
+            {
+                ConsoleHelper.WriteError("Text too short.");
+                InputHelper.PrintSeparator();
+                return;
+            }
+
+            for (int i = 1; i < text.Length; i += 2)
+            {
+                Console.Write(text[i]);
+            }
+
+            InputHelper.completedTasks++;
+            Console.WriteLine();
+            InputHelper.PrintSeparator();
+        }
+    }
+}
