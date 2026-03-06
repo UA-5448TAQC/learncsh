0f7ad9e3ab1c1ba6e2af8c88222337c87e62654f
Khrystyna Ismailova <hrystyna.brus@gmail.com>
2026-03-06 10:23:10 +0200

    hw03

---
+++ hw/hw03/Task5.cs
@@ -0,0 +1,42 @@
+﻿namespace Task5
+{
+    //enum Days { Monday = 1, Tuesday, Wednesday, Thursday, Friday, Sutarday, Sunday };
+    enum Month { January = 1, February, March, April, May, June, July, August, September, October, November, December };
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter day: ");
+            int day = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter day of the week (1-7): ");
+            //string Days = Console.ReadLine();
+
+            Console.Write("Enter month (1-12): ");
+            int monthNumber = int.Parse(Console.ReadLine());
+
+            if (monthNumber < 1 || monthNumber > 12)
+            {
+                Console.WriteLine("Invalid date.");
+                return;
+            }
+
+            Month month = (Month)monthNumber;
+
+            int maxDays = 31;
+
+            if (month == Month.February)
+                maxDays = 28;
+            else if (month == Month.April ||
+                     month == Month.June ||
+                     month == Month.September ||
+                     month == Month.November)
+                maxDays = 30;
+
+            bool isValid = day >= 1 && day <= maxDays;
+
+            Console.WriteLine(isValid ? "Valid date." : "Invalid date.");
+
+        }
+    }
+}
---
+++ hw/hw03/Task9.cs
@@ -0,0 +1,25 @@
+﻿namespace Task9;
+  struct RGB
+{
+    public byte red;
+    public byte green;
+    public byte blue;
+}
+internal class Program
+    {
+        static void Main(string[] args)
+    {
+        RGB white;
+        white.red = 255;
+        white.green = 255;
+        white.blue = 255;
+
+        RGB black;
+        black.red = 0;
+        black.green = 0;
+        black.blue = 0;
+
+        Console.WriteLine($"White: R={white.red}, G={white.green}, B={white.blue}");
+        Console.WriteLine($"Black: R={black.red}, G={black.green}, B={black.blue}");
+    }
+}
