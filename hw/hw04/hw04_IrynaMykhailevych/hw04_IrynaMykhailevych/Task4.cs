28589a3576a086ed22385650a5c54cd4fb896564
Iryna Mykhailevych <irynayanio@gmail.com>
2026-03-08 22:00:53 +0200

    Add HW04

---
+++ hw/hw04/hw04_IrynaMykhailevych/hw04_IrynaMykhailevych/Task4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hw04_IrynaMykhailevych
+{
+    internal class Task4
+    {
+        public static void Run() 
+        { 
+            int a = ReadInt("Enter a number 1: ");
+            int b = ReadInt("Enter a number 2: ");
+
+            int count = 0;
+
+            if (a > b)
+            {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Count: " + count);
+        }
+        static int ReadInt(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine();
+            int result;
+
+            while (!int.TryParse(input, out result))
+            {
+                Console.WriteLine("Invalid input. Try again:");
+                input = Console.ReadLine();
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ hw/hw04/hw04_IrynaMykhailevych/hw04_IrynaMykhailevych/Task6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace hw04_IrynaMykhailevych
+{
+    internal class Task6
+    {
+        public static void Run() 
+        {
+            Console.WriteLine("Enter a drink: ");
+            string drink = Console.ReadLine();
+
+            drink = drink.ToLower();
+
+            switch (drink)
+            {
+                case "coffee":
+                    Console.WriteLine("Drink: coffee, Price: 3$");
+                    break;
+                case "tea":
+                    Console.WriteLine("Drink: tea, Price: 2$");
+                    break;
+                case "juice":
+                    Console.WriteLine("Drink: juice, Price: 4$");
+                    break;
+                case "water":
+                    Console.WriteLine("Drink: water, Price: 1$");
+                    break;
+                default:
+                    Console.WriteLine("Unknown drink.");
+                    break;
+            }
+        }
+    }
+} 
