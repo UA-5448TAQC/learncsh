024dd0fd5dc0bff3a5aeee76e3f6386aecb9f053
Maryna Paziuk <maryna.paziuk@gmail.com>
2026-03-09 02:22:45 +0200

    hw03

---
+++ hw/hw03/mary-pzk/Task 7.cs
@@ -0,0 +1,39 @@
+﻿namespace HW_3
+{
+    internal class Task_7
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter hour (0-23):");
+            int hour;
+            if (!int.TryParse(Console.ReadLine(), out hour))
+            {
+                Console.WriteLine("Invalid input.");
+                return;
+            }
+
+            if (hour > 23 || hour < 0)
+            {
+                Console.WriteLine("Invalid hour.");
+                return;
+            }
+
+            if (hour >= 6 && hour <= 11)
+            {
+                Console.WriteLine("Good morning!");
+            }
+            else if (hour >= 12 && hour <= 17)
+            {
+                Console.WriteLine("Good afternoon!");
+            }
+            else if (hour >= 18 && hour <= 22)
+            {
+                Console.WriteLine("Good evening!");
+            }
+            else
+            {
+                Console.WriteLine("Good night!");
+            }
+        }
+    }
+}
