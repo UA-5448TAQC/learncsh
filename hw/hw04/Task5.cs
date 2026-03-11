31ebe4a61eb0559585c6025459a551658df9610f
Khrystyna Ismailova <hrystyna.brus@gmail.com>
2026-03-11 18:58:26 +0200

    HW04

---
+++ hw/hw04/Task5.cs
@@ -0,0 +1,19 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter some value: ");
+            string text = Console.ReadLine();
+
+
+            for (int i = 1; i < text.Length; i += 2)
+            {
+                Console.Write(text[i]);
+            }
+
+        }
+    }
+}
