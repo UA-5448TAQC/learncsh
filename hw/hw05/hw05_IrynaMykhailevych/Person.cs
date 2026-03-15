62d44b39311d0d93de43ecf28bd3c8696f22bded
Iryna Mykhailevych <irynayanio@gmail.com>
2026-03-15 23:18:57 +0200

    Add HW05

---
+++ hw/hw05/hw05_IrynaMykhailevych/Person.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw05_IrynaMykhailevych
+{
+    internal class Person
+    {
+        private string name;
+        private DateTime birthYear;
+
+        public string Name 
+        { 
+            get { return name; } 
+        }
+
+        public DateTime BirthYear 
+        { 
+            get { return birthYear; }
+        }
+
+        public Person()
+        {
+        }
+
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            return DateTime.Now.Year - birthYear.Year;
+        }
+        
+        public void Input()
+        {
+            Console.Write("Enter name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter birth year (yyyy): ");
+            int year;
+            while (!int.TryParse(Console.ReadLine(), out year) || year < 1900 || year > DateTime.Now.Year)
+            {
+                Console.Write("Invalid input. Please enter a valid birth year (yyyy): ");
+            }
+            birthYear = new DateTime(year, 1, 1);
+        }
+
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+
+        public override string ToString() 
+        {
+            return $"Name: {Name}, Age: {Age()}";
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public static bool operator ==(Person p1, Person p2)
+        {
+            if (ReferenceEquals(p1, p2))
+                return true;
+
+            if (p1 is null || p2 is null)
+                return false;
+
+            return p1.name == p2.name;
+        }
+
+        public static bool operator !=(Person p1, Person p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Person other)
+                return name == other.name;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return name.GetHashCode();
+        }
+    }
+}
---
+++ hw/hw05/hw05_IrynaMykhailevych/ProgramCar.cs
@@ -0,0 +1,57 @@
+﻿using System.Drawing;
+
+namespace hw05_IrynaMykhailevych
+{
+    internal class ProgramCar
+    {
+        public static void Main()
+        {
+            Car car1 = new Car();
+            Car car2 = new Car();
+            Car car3 = new Car();
+
+            Car[] cars = { car1, car2, car3 };
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine("---------------");
+                Console.WriteLine($"Enter data for car {i + 1}:");
+                cars[i].Input();
+            }
+
+            Console.WriteLine("---------------");
+            Console.WriteLine("Initial data: ");
+            car1.Print();
+            car2.Print();
+            car3.Print();
+            Console.WriteLine();
+
+            car1.ChangePrice(-10);
+            car2.ChangePrice(-10);
+            car3.ChangePrice(-10);
+
+            Console.WriteLine("After price change: ");
+            car1.Print();
+            car2.Print();
+            car3.Print();
+            Console.WriteLine();
+
+            Console.WriteLine("Enter a new color: ");
+            string newColor = Console.ReadLine();
+
+            foreach (Car car in cars)
+            {
+                if (car.Color.ToLower() == "white")
+                {
+                    car.Color = newColor;
+                }
+            }
+
+            Console.WriteLine("After repaint: ");
+            car1.Print();
+            car2.Print();
+            car3.Print();
+            Console.WriteLine();
+        }
+    }
+}
