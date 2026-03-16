4cb5e339078210b3a8752f5bc7465a1f08a1a5a4
Viktoriia <109147739+Viktoriia0106@users.noreply.github.com>
2026-03-16 15:33:36 +0200

    feat: додано файли ДЗ05 від Viktoriia0106

---
+++ hw/hw05/Viktoriia0106/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleAppHW05
+{
+    class Person
+    {
+        private string name;
+        private DateTime birthYear;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Person() { }
+
+        public Person(string name, DateTime birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        public int Age()
+        {
+            return DateTime.Now.Year - birthYear.Year;
+        }
+
+        public void Input()
+        {
+            Console.Write("Enter name: ");
+            name = Console.ReadLine().Trim().ToLower();
+            Console.Write("Enter birth year (YYYY): ");
+            int year = int.Parse(Console.ReadLine());
+            birthYear = new DateTime(year, 1, 1);
+        }
+
+        public void ChangeName(string newName)
+        {
+            name = newName;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {name}, Age: {Age()}";
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public static bool operator ==(Person a, Person b)
+        {
+            if (ReferenceEquals(a, b)) return true;
+            if (a is null || b is null) return false;
+            return a.name == b.name;
+        }
+
+        public static bool operator !=(Person a, Person b)
+        {
+            return !(a == b);
+        }
+    }
+}
---
+++ hw/hw05/Viktoriia0106/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleAppHW05
+{ class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Input();
+
+            Person p2 = new Person();
+            p2.Input();
+
+            Person p3 = new Person();
+            p3.Input();
+
+            Person p4 = new Person();
+            p4.Input();
+
+            Person p5 = new Person();
+            p5.Input();
+
+            Person p6 = new Person();
+            p6.Input();
+
+            if (p1 == p2 && p1 == p3 && p1 == p4 && p1 == p5 && p1 == p6) 
+            {
+                Console.WriteLine("Same people (same name)" + p1.Name); 
+                Console.WriteLine("Same people (same age)" + p1.Age());
+            }
+
+            else
+                {
+                    Console.WriteLine("Different paople");
+                }
+            Person[] people = { p1, p2, p3, p4, p5, p6 };
+            foreach (Person p in people)
+            {
+                if (p.Age() < 16)
+                {
+                    p.ChangeName("Very Young");
+                }
+                p.Output();
+            }
+        }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
