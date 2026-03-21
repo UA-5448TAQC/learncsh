11c5a37d896e0f852b01e04d85313596a7e17a9f
Iryna Mykhailevych <irynayanio@gmail.com>
2026-03-21 23:05:06 +0200

    Add HW06

---
+++ hw/hw06/hw06_IrynaMykhailevych/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace hw06_Task1_IrynaMykhailevych
+{
+    internal class Bird : IFlyable
+    {
+        public string Name { get; set; } = "";
+        public bool CanFly { get; set; }
+
+        public void Fly()
+        {
+            if (CanFly)
+            {
+                Console.WriteLine($"Bird {Name} is flying.");
+            }
+            else
+            {
+                Console.WriteLine($"Bird {Name} cannot fly.");
+            }
+        }
+    }
+}
---
+++ hw/hw06/hw06_Task4_IrynaMykhailevych/ProgramTask4.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace hw06_Task4_IrynaMykhailevych
+{
+    internal class ProgramTask4
+    {
+        static void Main()
+        {
+            Dictionary<uint, string> persons = new Dictionary<uint, string>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                uint id;
+                string name;
+                Console.WriteLine($"Enter ID for person {i + 1}:");
+                string inputId = Console.ReadLine() ?? "";
+                uint.TryParse(inputId, out id);
+                Console.WriteLine($"Enter name for person {i + 1}:");
+                name = Console.ReadLine() ?? "";
+                persons.Add(id, name);
+            }
+
+            uint searchId;
+            Console.WriteLine("Enter ID:");
+            string searchInput = Console.ReadLine() ?? "";
+            uint.TryParse(searchInput, out searchId);
+
+            if (persons.ContainsKey(searchId))
+            {
+                Console.WriteLine(persons[searchId]);
+            }
+            else
+            {
+                Console.WriteLine("ID not found.");
+            }
+        }
+    }
+}
