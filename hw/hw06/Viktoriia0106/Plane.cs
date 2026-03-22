acee34b5b17c0f9d1d4f4c95f98c29dde764088b
Viktoriia <109147739+Viktoriia0106@users.noreply.github.com>
2026-03-22 19:50:56 +0200

    feat: додано ДЗ06 Romanova Viktoriia

---
+++ hw/hw06/Viktoriia0106/Plane.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleAppVRHW06
+{
+    class Plane : IFlyable
+    {
+        public string mark;
+        public int highFly;
+        public Plane(string mark, int highFly)
+        {
+            this.mark = mark;
+            this.highFly = highFly;
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"{mark} is flying at an altitude of {highFly} feet");
+        }
+    }
+}
