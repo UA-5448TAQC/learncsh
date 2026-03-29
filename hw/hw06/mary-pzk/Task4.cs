a2b17d1cba7793be84bfa72e247e1ec7739c30df
Maryna Paziuk <maryna.paziuk@gmail.com>
2026-03-29 19:20:50 +0300

    hw06

---
+++ hw/hw06/mary-pzk/Task4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<uint, string> persons = new Dictionary<uint, string>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.Write("Enter ID: ");
+                uint id = uint.Parse(Console.ReadLine()!);
+
+                Console.Write("Enter Name: ");
+                string name = Console.ReadLine()!;
+
+                persons.Add(id, name);
+            }
+
+
+            Console.Write("Enter ID to search: ");
+            uint idSearch = uint.Parse(Console.ReadLine()!);
+
+            if (persons.ContainsKey(idSearch))
+            {
+                Console.WriteLine("Name: " + persons[idSearch]);
+            }
+            else
+            {
+                Console.WriteLine("ID not found.");
+            }
+        }
+    }
+}
