b09568f5f61930d1634c1357669faaec083511ec
HorlovaSofiia <sofgorlova3@gmail.com>
2026-04-24 12:59:41 +0300

    HW09

---
+++ hw/hw09/HorovaSofiia/ConsoleApp1/PointTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1
+{
+    public class PointTests
+    {
+        [Test]
+        public void ToString_ReturnsCorrectFormat()
+        {
+            Point point = new Point(3, 4);
+
+            string result = point.ToString();
+
+            Assert.That(result, Is.EqualTo("(3,4)"));
+        }
+
+        [Test]
+        public void DistanceTo_WhenSamePoint_ReturnsZero()
+        {
+            Point first = new Point(2, 2);
+            Point second = new Point(2, 2);
+
+            double result = first.DistanceTo(second);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void DistanceTo_WithPositiveCoordinates_ReturnsCorrectDistance()
+        {
+            Point first = new Point(0, 0);
+            Point second = new Point(3, 4);
+
+            double result = first.DistanceTo(second);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void DistanceTo_WithNegativeCoordinates_ReturnsCorrectDistance()
+        {
+            Point first = new Point(-1, -1);
+            Point second = new Point(-4, -5);
+
+            double result = first.DistanceTo(second);
+
+            Assert.That(result, Is.EqualTo(5));
+        }
+    }
+}
---
+++ hw/hw09/HorovaSofiia/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Triangle triangle1 = new Triangle(
+                new Point(0, 0),
+                new Point(3, 0),
+                new Point(0, 4));
+
+            Triangle triangle2 = new Triangle(
+                new Point(5, 5),
+                new Point(8, 5),
+                new Point(5, 9));
+
+            Triangle triangle3 = new Triangle(
+                new Point(-2, -1),
+                new Point(-4, 3),
+                new Point(1, 2));
+
+            ShapeGroup group = new ShapeGroup();
+
+            group.AddTriangle(triangle1);
+            group.AddTriangle(triangle2);
+            group.AddTriangle(triangle3);
+
+            Console.WriteLine("All triangles:");
+            foreach (Triangle triangle in group.GetAll())
+            {
+                triangle.Print();
+                Console.WriteLine();
+            }
+
+            Triangle? closestTriangle = group.FindTriangleClosestToOrigin();
+
+            Console.WriteLine("Triangle with vertex closest to origin:");
+            if (closestTriangle is not null)
+            {
+                closestTriangle.Print();
+            }
+            else
+            {
+                Console.WriteLine("Shape group is empty.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
